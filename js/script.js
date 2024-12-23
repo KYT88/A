ea3e14d1ddaaaede85047fb98b39bb57df4029d5
KYT88 <158803821+KYT88@users.noreply.github.com>
2024-12-23 10:28:05 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector(".nav_hamburger");
+const linkContainer = document.querySelector(".nav_menu");
+const links = document.querySelectorAll(".nav_menu_link");
+
+hamburger.addEventListener("click", () =>{
+    linkContainer.classList.toggle("active");
+    hamburger.classList.toggle("active");
+});
+
+window.addEventListener("resize", () =>{
+    if(window.matchMedia("(max-width: 550px)").matches)
+    {
+        closeMenu();
+    }
+});
+
+if(window.matchMedia("(max-width: 550px").matches)
+{
+    closeMenu();
+}
+
+    function closeMenu() {
+        links.forEach(link => {
+            link.addEventListener("click", () => {
+                linkContainer.classList.remove("active");
+                hamburger.classList.remove("active");
+            });
+        });
+    }
+    
